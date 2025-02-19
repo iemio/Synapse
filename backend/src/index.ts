0bb3abf862e219c32f0c82b2f0684e6869fdcfd5
iemio <deeplearner4u@gmail.com>
2025-02-19 18:50:50 +0530

    Basic setup and error handling

---
+++ backend/src/index.ts
@@ -0,0 +1,49 @@
+import "dotenv/config";
+import express, { NextFunction, Request, Response } from "express";
+import cors from "cors";
+import session from "cookie-session";
+import { config } from "./config/app.config";
+import connectDatabase from "./config/database.config";
+import { errorHandler } from "./middlewares/errorHandler.middleware";
+import { HTTPSTATUS } from "./config/http.config";
+import { asyncHandler } from "./middlewares/asyncHandler.middleware";
+import { BadRequestException } from "./utils/appError";
+import { ErrorCodeEnum } from "./enums/error-code.enum";
+
+const app = express();
+const BASE_PATH = config.BASE_PATH;
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+    session({
+        name: "session",
+        keys: [config.SESSION_SECRET],
+        maxAge: 24 * 60 * 60 * 1000,
+        secure: config.NODE_ENV === "production",
+        httpOnly: true,
+        sameSite: "lax",
+    })
+);
+
+app.get(
+    `/`,
+    asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+        throw new BadRequestException(
+            "This is a bad request",
+            ErrorCodeEnum.AUTH_INVALID_TOKEN
+        );
+        return res.status(HTTPSTATUS.OK).json({
+            message: "Hello Subscribe to the channel & share",
+        });
+    })
+);
+
+app.listen(config.PORT, async () => {
+    console.log(
+        `Server listening on port ${config.PORT} in ${config.NODE_ENV}`
+    );
+    await connectDatabase();
+});
