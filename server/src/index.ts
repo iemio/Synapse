1eb5ffcf9dd2b0b71fddcd35d55c4568d8e5311f
iemio <deeplearner4u@gmail.com>
2025-02-20 02:22:04 +0530

    Complted basline

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,6 +10,16 @@ import { asyncHandler } from "./middlewares/asyncHandler.middleware";
 import { BadRequestException } from "./utils/appError";
 import { ErrorCodeEnum } from "./enums/error-code.enum";
 
+import "./config/passport.config";
+import passport from "passport";
+import authRoutes from "./routes/auth.route";
+import userRoutes from "./routes/user.route";
+import isAuthenticated from "./middlewares/isAuthenticated.middleware";
+import workspaceRoutes from "./routes/workspace.route";
+import memberRoutes from "./routes/member.route";
+import projectRoutes from "./routes/project.route";
+import taskRoutes from "./routes/task.route";
+
 const app = express();
 const BASE_PATH = config.BASE_PATH;
 
@@ -28,6 +38,16 @@ app.use(
     })
 );
 
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(
+    cors({
+        origin: config.FRONTEND_ORIGIN,
+        credentials: true,
+    })
+);
+
 app.get(
     `/`,
     asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
@@ -40,6 +60,15 @@ app.get(
         });
     })
 );
+
+app.use(`${BASE_PATH}/auth`, authRoutes);
+app.use(`${BASE_PATH}/user`, isAuthenticated, userRoutes);
+app.use(`${BASE_PATH}/workspace`, isAuthenticated, workspaceRoutes);
+app.use(`${BASE_PATH}/member`, isAuthenticated, memberRoutes);
+app.use(`${BASE_PATH}/project`, isAuthenticated, projectRoutes);
+app.use(`${BASE_PATH}/task`, isAuthenticated, taskRoutes);
+
+app.use(errorHandler);
 
 app.listen(config.PORT, async () => {
     console.log(
